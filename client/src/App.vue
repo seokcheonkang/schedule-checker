b081b76573aa0d3c007454736142349fa2a78eef
KimYunBeom <yunbeom.kim@bespinglobal.com>
2022-04-15 14:43:51 +0900

    install bootstrap
    update :class by click event

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,16 +1,29 @@
-<script setup></script>
+<script setup>
+let active = 'nav-link';
+
+function setActive() {
+  active = 'nav-link active';
+}
+</script>
 
 <template>
-  <header></header>
-  <nav>
-    <span>
-      <router-link to="/">Home</router-link>
-    </span>
-    <span> | </span>
-    <span>
-      <router-link to="/about">About</router-link>
-    </span>
-  </nav>
+  <header>
+    <div class="navbar navbar-expand-lg navbar-dark bg-dark overflow-hidden">
+      <div class="container-fluid">
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <router-link to="/" :class="active" @click="setActive">Home</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link to="/about" :class="active" @click="setActive">About</router-link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </header>
+  <nav></nav>
   <main>
     <router-view />
   </main>
