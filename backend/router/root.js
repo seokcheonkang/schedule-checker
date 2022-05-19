1a1a21d45b9b038fea0ad8d5e8b29356f6cfffce
KimYunBeom <yunbeom.kim@bespinglobal.com>
2022-05-19 14:20:04 +0900

    divide router to separete files in backend

---
+++ backend/router/root.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+
+const os = require('os');
+
+router.get('/', (req, res) => {
+  const result = {
+    userAgent: req.headers['user-agent'],
+    hostname: os.hostname(),
+  };
+
+  res.send(result);
+});
+
+module.exports = router;
