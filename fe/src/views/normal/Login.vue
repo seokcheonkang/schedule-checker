546211a9a2cd7f299d515025273995391f951cd0
KimYunBeom <yunbeom.kim@bespinglobal.com>
2022-07-06 17:03:06 +0900

    route home after login

--- fe/src/views/normal/Login.vue
+++ fe/src/views/normal/Login.vue
@@ -1,6 +1,6 @@
 <script setup>
-import { onBeforeMount, onMounted, reactive } from 'vue';
-import { useRoute } from 'vue-router';
+import { onBeforeMount, onMounted, watchEffect } from 'vue';
+import { useRoute, useRouter } from 'vue-router';
 
 import GoogleLogin from './GoogleLogin.vue';
 
@@ -15,27 +15,22 @@ import { useLoginStore } from '@/store/login.js';
 
 // route
 const route = useRoute();
+const router = useRouter();
 
 // store
 const loginStore = useLoginStore();
 
-// state
-const state = reactive({
-  userInfo: {
-    userId: null,
-  },
-  isLogin: loginStore.isLogin,
+watchEffect(() => {
+  if (loginStore.isLogin) {
+    router.push('/');
+  }
 });
 
 onBeforeMount(() => {
   LOGD(route.name);
 });
 
-onMounted(() => {
-  if (loginStore.isLogin) {
-    state.userInfo.userId = loginStore.userInfo.id;
-  }
-});
+onMounted(() => {});
 </script>
 
 <template>
@@ -44,11 +39,11 @@ onMounted(() => {
     <div class="row align-items-center py-1">
       <div class="col-md-10 mx-auto col-lg-10">
         <form class="p-4 p-md-4 border rounded-3 bg-light">
-          <div v-if="!state.isLogin">
+          <div v-if="!loginStore.isLogin">
             <GoogleLogin />
             <hr class="my-4" />
           </div>
-          <div v-else>환영합니다. {{ state.userInfo.userId }}</div>
+          <div v-else>환영합니다. {{ loginStore.userInfo.name }}님.</div>
         </form>
       </div>
     </div>
