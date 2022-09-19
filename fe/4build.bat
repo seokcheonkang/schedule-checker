67c8377eca60e9c698ce505878c85bd0f795c59b
KimYunBeom <yunbeom.kim@bespinglobal.com>
2022-09-19 13:06:50 +0900

    update build dev

--- fe/4build.bat
+++ fe/4build.bat
@@ -1,8 +1,8 @@
 docker login
-start /b npm run build
+start /b npm run build-dev
 timeout /t 20 /nobreak
-start /b docker build --tag idealful/example:5.0.0 C:\data\dev\workspace3\schedule-checker\fe
+start /b docker build --tag idealful/example:4.0.0 C:\data\dev\workspace3\schedule-checker\fe
 timeout /t 5 /nobreak
-docker push idealful/example:5.0.0
-docker rmi idealful/example:5.0.0
+docker push idealful/example:4.0.0
+docker rmi idealful/example:4.0.0
 rmdir /s /q dist
