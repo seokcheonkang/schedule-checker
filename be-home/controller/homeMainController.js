3811b4f4a203f848c32394ce7c56859cb0dcb35b
KimYunBeom <yunbeom.kim@bespinglobal.com>
2022-10-27 11:30:58 +0900

    fixing apps to prevent crashes #2

--- be-home/controller/homeMainController.js
+++ be-home/controller/homeMainController.js
@@ -14,13 +14,25 @@ const LOGD = require(`${MIDDLEWARE_PATH}/logd`);
 router.get('/', (req, res) => {
   LOGD(req.originalUrl);
 
-  const result = { userAgent: req.headers['user-agent'], hostname: os.hostname() };
+  let response = {};
+  try {
+    const result = { userAgent: req.headers['user-agent'], hostname: os.hostname() };
 
-  const response = { code: 200, message: '조회 성공', result };
+    response.code = 200; // OK
+    if (!result) {
+      response.code = 204; // No Content
+    }
+    response.result = result;
+    response.message = '조회 성공';
 
-  LOGD(JSON.stringify(response));
-
-  res.status(200).json(response);
+    LOGD(JSON.stringify(response));
+  } catch (error) {
+    response.code = 500; // Internal Server Error
+    response.result = 'Error occured. Ask the administrator.';
+    LOG('[ERROR]', JSON.stringify(error?.toString()));
+  } finally {
+    res.status(200).json(response);
+  }
 });
 
 module.exports = router;
